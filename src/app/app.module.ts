7767af62fc76a0af43c231bb246f039c452f67c4
ZloiY <NoRFolf1000@gmail.com>
2017-06-23 18:07:03 +0300

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {TodoListComponent} from './todolist/todolist.component';
-import {CompleteTasksPipe} from './todolist/todolist.complete-tasks.pipe';
-import {ActiveTasksPipe} from './todolist/todolist.active-tasks.pipe';
+import { TodoListComponent } from './todolist/todolist.component';
+import { CompleteTasksPipe } from './todolist/pipes/todolist.complete-tasks.pipe';
+import { ActiveTasksPipe } from './todolist/pipes/todolist.active-tasks.pipe';
+import { TodolistInputComponent } from './todolist/todolist-input/todolist-input.component';
+import { TodolistSortComponent } from './todolist/todolist-sort/todolist-sort.component';
+import { TodolistTaskelementComponent } from './todolist/todolist-taskelement/todolist-taskelement.component';
+import { TodolistCheckallDelComponent } from './todolist/todolist-checkall-del/todolist-checkall-del.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     CompleteTasksPipe,
-    ActiveTasksPipe
+    ActiveTasksPipe,
+    TodolistInputComponent,
+    TodolistSortComponent,
+    TodolistTaskelementComponent,
+    TodolistCheckallDelComponent,
   ],
   imports: [
     BrowserModule
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ElementRef, ViewChild, Renderer2} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task} from '../task';
 
 @Component({
@@ -8,24 +8,14 @@ import {Task} from '../task';
 })
 
 export class TodoListComponent implements OnInit {
-  @ViewChild('allTasks') allTasks: ElementRef;
-  @ViewChild('completeTasks') completeTasks: ElementRef;
-  @ViewChild('activeTasks') activeTasks: ElementRef;
   task: Task;
   tasks: Task[];
   activeItems: number;
-  allCheckbox: boolean;
   allCheck: boolean;
   activeCheck: boolean;
   completeCheck: boolean;
 
-  constructor(private rd: Renderer2) {}
-
-  onAddBtnClick(newTask: string): void {
-    if (newTask.length === 0) {
-      alert('You must entry task name.');
-      return;
-    }
+  onAdd(newTask: string): void {
     this.task = {
       name: newTask,
       check: false,
@@ -34,65 +24,52 @@ export class TodoListComponent implements OnInit {
     this.getActiveTasks();
   }
 
-  onCheckClick(task: Task, checked: boolean): void {
-    task.check = checked;
+  onAll() {
+    this.allCheck = true;
+    this.completeCheck = false;
+    this.activeCheck = false;
+  }
+
+  onComplete() {
+    this.allCheck = false;
+    this.completeCheck = true;
+    this.activeCheck = false;
+  }
+
+  onActive() {
+    this.allCheck = false;
+    this.completeCheck = false;
+    this.activeCheck = true;
+  }
+
+  onCheck() {
     this.getActiveTasks();
   }
 
-  onCloseClick(closeTask: Task): void {
+  onClose(closeTask: Task) {
     this.tasks = this.tasks.filter((task) => task !== closeTask);
     this.getActiveTasks();
   }
 
-  onAllClick() {
-    this.allCheck = true;
-    this.activeCheck = false;
-    this.completeCheck = false;
-    this.rd.addClass(this.allTasks.nativeElement, 'radio-btn-label-selected');
-    this.rd.removeClass(this.completeTasks.nativeElement, 'radio-btn-label-selected');
-    this.rd.removeClass(this.activeTasks.nativeElement, 'radio-btn-label-selected');
-  }
-
-  onCompleteClick() {
-    this.completeCheck = true;
-    this.allCheck = false;
-    this.activeCheck = false;
-    this.rd.addClass(this.completeTasks.nativeElement, 'radio-btn-label-selected');
-    this.rd.removeClass(this.allTasks.nativeElement, 'radio-btn-label-selected');
-    this.rd.removeClass(this.activeTasks.nativeElement, 'radio-btn-label-selected');
-  }
-
-  onActiveClick() {
-    this.activeCheck = true;
-    this.completeCheck = false;
-    this.allCheck = false;
-    this.rd.addClass(this.activeTasks.nativeElement, 'radio-btn-label-selected');
-    this.rd.removeClass(this.allTasks.nativeElement, 'radio-btn-label-selected');
-    this.rd.removeClass(this.completeTasks.nativeElement, 'radio-btn-label-selected');
-  }
-
-  onCheckUncheck() {
+  onCheckUncheck(allCheckbox: boolean) {
     this.tasks.forEach((task) => {
-      task.check = !this.allCheckbox;
+      task.check = !allCheckbox;
     });
-    this.allCheckbox = !this.allCheckbox;
     this.getActiveTasks();
   }
 
   onDelChecked() {
     this.tasks = this.tasks.filter((task) => !task.check);
-    this.allCheckbox = false;
   }
 
-  getActiveTasks(): void {
+  getActiveTasks() {
     const activeTasks = this.tasks.filter((task) => !task.check);
     this.activeItems = activeTasks.length;
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.tasks = [];
-    this.allCheck = true;
     this.getActiveTasks();
-    this.onAllClick()
   }
+
 }
