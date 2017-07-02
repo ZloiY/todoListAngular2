2a26306b8cbce6243f3d32931b06794c2af363f5
ZloiY <NoRFolf1000@gmail.com>
2017-07-02 11:28:28 +0300

    feat: add validation for input

--- src/app/todo-list/tdlist-input/tdlist-input.component.ts
+++ src/app/todo-list/tdlist-input/tdlist-input.component.ts
@@ -1,19 +1,22 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tdlist-input',
   templateUrl: 'tdlist-input.component.html',
   styleUrls: ['tdlist-input.component.scss'],
 })
-export class TodoListInputComponent {
+export class TodoListInputComponent implements OnInit{
 
   @Output() onAdd = new EventEmitter<string>();
+  taskForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
 
   onAddBtnClick(taskInput) {
     const taskName = (<HTMLInputElement>taskInput);
-    if (taskName.value === '') {
-      alert('You must entry task name.');
-      return;
+    if (this.formErrors.taskName) {
+      return
     }
     this.onAdd.emit(taskName.value);
     taskName.value = '';
@@ -24,5 +27,51 @@ export class TodoListInputComponent {
       this.onAddBtnClick((<HTMLInputElement>event.target));
     }
   }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.taskForm = this.formBuilder.group({
+      'taskName':['',
+        [
+          Validators.required,
+          Validators.maxLength(50),
+        ]],
+    });
+
+    this.taskForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged()
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.taskForm) {
+      return;
+    }
+    const form = this.taskForm;
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = { 'taskName': '' };
+
+  validationMessages = {
+    'taskName': {
+      'required':'Task name is required.',
+      'maxlength': 'Task name must be shorter then 50 charachters',
+    },
+  };
 
 }
