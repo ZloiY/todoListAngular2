f6f6dbdf9063cbdcbf75e1bb7f515254a1e457c7
ZloiY <NoRFolf1000@gmail.com>
2017-07-11 14:22:57 +0300

    feat: setup angular-cli proxy

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   serverUrls : {
-    tasksUrl: 'http://localhost:9000/tasks',
-    taskUrl: 'http://localhost:9000/tasks/task',
-    userUrl: 'http://localhost:9000/user',
-    registrationUrl: 'http://localhost:9000/user',
-    authenticationUrl: 'http://localhost:9000/user/login',
-    logoutUrl: 'http://localhost:9000/user/logout',
+    tasksUrl: '/tasks',
+    taskUrl: '/tasks/task',
+    userUrl: '/user',
+    registrationUrl: '/user',
+    authenticationUrl: '/user/login',
+    logoutUrl: '/user/logout',
   },
 };
