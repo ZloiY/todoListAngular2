16a71c6b7d090e77646a00ae579f469060cf12d6
ZloiY <NoRFolf1000@gmail.com>
2017-07-24 13:40:32 +0300

    refactor: remove canLoad

--- src/app/authentication/auth-guard.service.ts
+++ src/app/authentication/auth-guard.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, CanLoad} from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate, CanLoad {
+export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthenticationService) {}
 
   canActivate(): boolean {
     return this.authService.isLoggedIn();
-  }
-
-  canLoad(): boolean {
-    return !this.authService.isLoggedIn();
   }
 }
