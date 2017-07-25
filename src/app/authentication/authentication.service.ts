a22fa0c09a78d437c5f4f18c6c668cfaa9f3f05e
ZloiY <NoRFolf1000@gmail.com>
2017-07-25 11:38:47 +0300

    fix: change local storage to sessionStorage

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -8,9 +8,7 @@ import {User} from "../core/user.model";
 @Injectable()
 export class AuthenticationService implements OnInit {
 
-  ngOnInit() {
-    localStorage.setItem('token', '');
-  }
+  ngOnInit() {}
 
   private headers = new Headers({'Content-Type': 'application/json'});
   private params = new URLSearchParams();
@@ -28,7 +26,7 @@ export class AuthenticationService implements OnInit {
     return this.http
       .post(environment.serverUrls.authenticationUrl, user, {headers: this.headers})
       .map((token) => {
-      localStorage.setItem('token', token.json() as string);
+      sessionStorage.setItem('token', token.json() as string);
       this.params.set('token', token.json() as string);
       })
       .catch(this.handleError);
@@ -38,24 +36,24 @@ export class AuthenticationService implements OnInit {
     return this.http
       .post(environment.serverUrls.logoutUrl, null, {headers: this.headers, search: this.params})
       .map(() => {
-        localStorage.setItem('token', '');
+        sessionStorage.setItem('token', '');
       })
       .catch(this.handleError)
   }
 
   getCurrentUser(): Observable<User> {
-    this.params.set('token', localStorage.getItem('token'));
+    this.params.set('token', sessionStorage.getItem('token'));
     return this.http.get(environment.serverUrls.userUrl, {search: this.params})
       .map((user) => user.json() as User)
       .catch(this.handleError)
   }
 
   getAccessesToken() {
-    return localStorage.getItem('token');
+    return sessionStorage.getItem('token');
   }
 
   isLoggedIn() {
-    if (localStorage.getItem('token')) {
+    if (sessionStorage.getItem('token')) {
       return true;
     }
     return false;
