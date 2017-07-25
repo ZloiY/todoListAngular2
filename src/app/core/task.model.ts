e53cfcc3fdc3470fa568dd4bb07b666f0f791fcd
ZloiY <NoRFolf1000@gmail.com>
2017-07-25 18:02:33 +0300

    feat: add constructor

--- src/app/core/task.model.ts
+++ src/app/core/task.model.ts
@@ -2,4 +2,10 @@ export  class Task {
   id: number;
   name: string;
   complete: boolean;
+
+  constructor(id, name, complete) {
+    this.id = id;
+    this.name = name;
+    this.complete = complete;
+  }
 }
